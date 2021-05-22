bcaa985ca5ebd33d08f44e6af834fc2559355fac
pablo <pcollars@alumnos.unex.es>
2021-05-22 12:57:33 +0200

    Proyecto integrado con qr

---
+++ Assets/Scripts/ToogleCutoutBox.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToogleCutoutBox : MonoBehaviour
+{
+    [SerializeField] GameObject realBox;
+    [SerializeField] GameObject fakeBox;
+    [SerializeField] GameObject container;
+
+    private BoxCollider containerCollider;
+    private ObjectManipulator containerManipulator;
+    private BoundsControl containerControl;
+
+    private bool isActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        containerCollider = container.GetComponent<BoxCollider>();
+        containerManipulator = container.GetComponent<ObjectManipulator>();
+        containerControl = container.GetComponent<BoundsControl>();
+    }
+
+    public void ChangeStatus()
+    {
+        Debug.Log("Cambiando");
+        if (isActive)
+        {
+            realBox.SetActive(false);
+            fakeBox.SetActive(false);
+            containerCollider.enabled =true;
+            containerManipulator.enabled =true;
+            containerControl.enabled =true;
+        }
+        else
+        {
+            realBox.SetActive(true);
+            fakeBox.SetActive(true);
+            containerCollider.enabled =false;
+            containerManipulator.enabled =false;
+            containerControl.enabled =false;
+        }
+        isActive = !isActive;
+    }
+}
---
+++ Assets/Scripts/ZipLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZipLoader : MonoBehaviour
+{
+
+    FileDownloader fileDownloader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FileDownloader fileDownloader = new FileDownloader();
+
+        // This callback is triggered for DownloadFileAsync only
+        fileDownloader.DownloadProgressChanged += (sender, e) => Debug.Log("Progress changed " + e.BytesReceived + " " + e.TotalBytesToReceive);
+        // This callback is triggered for both DownloadFile and DownloadFileAsync
+        fileDownloader.DownloadFileCompleted += (sender, e) => Debug.Log("Download completed");
+
+        //fileDownloader.DownloadFileAsync("https://drive.google.com/file/d/1_u9UWNOvtBi8KB3skLcq6pwnyAEyCezS/view?usp=sharing", @"C:\Users\Pablo\papeles\unex\cuarto\TFG\archivo.zip");
+    }
+
+
+}
